3406371eaf9c6404e2285542cb4533fa925709db
Mohammed Hassan <mr.math2024@gmail.com>
2023-04-07 23:42:12 +0200

    fix host issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Tabs from './components/Tabs';
 import Layout from './components/Layout';
 import Details from './components/Details';
 
-export const host = "http://192.168.1.110:3000/";
+// export const host = "http://192.168.1.110:3000/";
+export const host = `${window.location.protocol}//${window.location.host}/`;
+
+
 
 export default function App() {
     return (
@@ -16,8 +19,8 @@ export default function App() {
                         <Route index element={<Details />} />
                         <Route path=":tab" element={<Details />} />
                     </Route>
+                    <Route path='undefined' element={<Navigate to='mercury' />} />
                 </Route>
-                <Route path='*' element={<Navigate to='mercury' />} />
             </Routes>
         </BrowserRouter>
     );
--- src/api.ts
+++ src/api.ts
@@ -2,15 +2,8 @@ import { host } from "./App";
 import { IPlanet } from "./types";
 
 export const getData = async (): Promise<IPlanet[]> => {
-    let planets;
-
-    if (localStorage['planets'] === undefined) {
-        const res = await fetch(host + 'data.json');
-        planets = await res.json();
-        localStorage.setItem('planets', JSON.stringify(planets));
-    } else {
-        planets = JSON.parse(localStorage['planets']);
-    }
+    const res = await fetch('/data.json');
+    const planets = await res.json();
 
     return planets;
 }
